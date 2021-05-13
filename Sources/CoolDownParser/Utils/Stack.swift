0ae666b5a48931c403f8688db2967d90c6e6c905
Philip Niedertscheider <dev@philpri.me>
2021-05-13 13:34:09 +0200

    Added documentation

--- Sources/CoolDownParser/Utils/Stack.swift
+++ Sources/CoolDownParser/Utils/Stack.swift
@@ -1,13 +1,19 @@
+/// Sequence which work as a "Last In, First Out" (LIFO) data structure.
+///
+/// In a stack, each item is placed on top of the previous item, one at a time.
+/// Items can be removed from either the top of the stack (FILO) or from the bottom of the stack FIFO.
 struct Stack<Element> {
 
     var elements: [Element]
 
     // MARK: Initializers
 
+    /// Creates a stack with no elements inside
     init() {
         self.elements = []
     }
 
+    /// Creates a stack with the given elements
     init(elements: [Element]) {
         self.elements = elements
     }
@@ -64,24 +70,47 @@ extension Stack: Collection {
 
     // MARK: - Collection
 
+    /// Indicies are
     typealias Index = Int
 
+    /// The position of the first element in a nonempty stack.
     var startIndex: Int {
-        elements.startIndex
+        0
     }
 
+    /// The stack's "past the end" position---that is, the position one greater
+    /// than the last valid subscript argument.
+    ///
+    /// When you need a range that includes the last element of an array, use the
+    /// half-open range operator (`..<`) with `endIndex`. The `..<` operator
+    /// creates a range that doesn't include the upper bound, so it's always
+    /// safe to use with `endIndex`. For example:
+    ///
+    ///     let numbers: Stack<Int> = [10, 20, 30, 40, 50]
+    ///     if let i = numbers.firstIndex(of: 30) {
+    ///         print(numbers[i ..< numbers.endIndex])
+    ///     }
+    ///     // Prints "[30, 40, 50]"
+    ///
+    /// If the stack is empty, `endIndex` is equal to `startIndex`.
     var endIndex: Int {
         elements.endIndex
     }
 
+    /// Returns the position immediately after the given index.
+    ///
+    /// - Parameter i: A valid index of the collection. `i` must be less than `endIndex`.
+    /// - Returns: The index immediately after `i`.
     func index(after index: Int) -> Int {
         elements.index(after: index)
     }
 
+    /// See `Array.subscript(bounds:)`
     subscript(bounds: Range<Int>) -> ArraySlice<Stack<Element>> {
         ArraySlice([Stack(Array(elements[bounds]))])
     }
 
+    /// See `Array.subscript(position:)`
     subscript(position: Int) -> Element? {
         elements[position]
     }
